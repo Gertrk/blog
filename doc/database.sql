cd1b5790be93037e90ee922b549605d38dc789d5
Gertrk <gert.rk1@hotmail.com>
2014-03-17 13:23:40 +0200

    Add framework files Customize project settings Rename welcome controller to posts Add posts listing functionality

--- doc/database.sql
+++ doc/database.sql
@@ -1,29 +1,49 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 4.0.9
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2013 at 10:20 AM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Loomise aeg: Märts 17, 2014 kell 12:22 PL
+-- Serveri versioon: 5.6.14
+-- PHP versioon: 5.5.6
 
 SET FOREIGN_KEY_CHECKS=0;
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-
 --
--- Database: `halo`
+-- Andmebaas: `blog`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Tabeli struktuur tabelile `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE IF NOT EXISTS `post` (
+  `post id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `post subject` varchar(255) NOT NULL,
+  `post text` text NOT NULL,
+  `post created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`post id`),
+  KEY `user id` (`user id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
+
+--
+-- Andmete tõmmistamine tabelile `post`
+--
+
+INSERT INTO `post` (`post id`, `post subject`, `post text`, `post created`, `user id`) VALUES
+(0, 'jaskb', 'asjfhbasjf', '2014-01-26 17:29:36', 1),
+(1, 'ytjuty', 'awerawer', '2014-01-26 17:29:59', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `user`
 --
 
 DROP TABLE IF EXISTS `user`;
@@ -36,13 +56,19 @@ CREATE TABLE IF NOT EXISTS `user` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Dumping data for table `user`
+-- Andmete tõmmistamine tabelile `user`
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password`, `deleted`) VALUES
 (1, 'demo', 'demo', 0);
-SET FOREIGN_KEY_CHECKS=1;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Tõmmistatud tabelite piirangud
+--
+
+--
+-- Piirangud tabelile `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user id`) REFERENCES `user` (`user_id`);
+SET FOREIGN_KEY_CHECKS=1;
