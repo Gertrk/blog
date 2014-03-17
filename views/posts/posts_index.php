e8716df0c2490ea47df611252cc392137b4eff08
Gertrk <gert.rk1@hotmail.com>
2014-03-17 17:15:28 +0200

    Add framework files Customize project settings Rename welcome controller to posts Add posts listing functionality Add single post viewing functionality

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,12 +1,13 @@
 <?foreach( $posts as $post ):?>
 <div class="span8">
 
-    <h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>">Alice in Wonderland, part dos</a></h1>
-    <p>'You ought to be ashamed of yourself for asking such a simple question,' added the Gryphon; and then they both sat silent and looked at poor Alice, who felt ready to sink into the earth. At last the Gryphon said to the Mock Turtle, 'Drive on, old fellow! Don't be all day about it!' and he went on in these words:
-        'Yes, we went to school in the sea, though you mayn't believe it—'
-        'I never said I didn't!' interrupted Alice.
-        'You did,' said the Mock Turtle.</p>
+    <h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post["post subject"]?></a></h1>
+    <p><?=$post["post_text"]?></p>
+    <div>
+
     <div>
         <span class="badge badge-success">Posted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
     </div>
    <?endforeach?>
+<?foreach ($tags[$post['post_id']]as $tag):?>
+    <a href="#"><span class="label" style="background-color:#5bc0de"><?=$tag?></span></a> <? endforeach?>
--- views/tags/tags_index.php
+++ views/tags/tags_index.php
@@ -5,14 +5,16 @@
  * Date: 17.03.14
  * Time: 15:33
  */
+$tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag");
+    foreach ($tags as $tag) {
+    $this->tags[$tag['post_id']][] = $tag['tag_name'];
+    }
 <?foreach($tags as $tag):?>
     <a href="<?=BASE_URL?>tags/view/<?=$tag['tag_name']?>" class="list-group-item"><span class="badge"><?=$tag['count']?></span>
         <?=$tag['tag_name']?>
     </a>
     <div class="list-group">
-        <a href="#" class="list-group-item active">
-            Cras justo odio
-        </a>
+        <a href="#" class="list-group-item active">Cras justo odio</a>
         <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
         <a href="#" class="list-group-item">Morbi leo risus</a>
         <a href="#" class="list-group-item">Porta ac consectetur ac</a>
