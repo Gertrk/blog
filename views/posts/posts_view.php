4ebe9287be8cf1682c618de7dfe10a91a1c7682e
Gertrk <gert.rk1@hotmail.com>
2014-04-02 18:04:13 +0300

    Add framework files Customize project settings Rename welcome controller to posts Add posts listing functionality Add single post viewing functionality

--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -4,4 +4,3 @@
     <div>
         <span class="badge badge-success">Posted <?=$post["post created"]?></span>
     </div>
-<?foreach ($tags as $tag):?><a href ="#"><span class="label label-info"><?=$tag['tag_name']?></span></a> <? endforeach?>
