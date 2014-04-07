7fb618f500ed1cd45e95b5fb2b7801a101cdcfa9
Gertrk <gert.rk1@hotmail.com>
2014-04-07 16:48:07 +0300

    Add framework files Customize project settings Rename welcome controller to posts Add posts listing functionality Add single post viewing functionality

--- controllers/tags.php
+++ controllers/tags.php
@@ -9,7 +9,7 @@ class tags extends Controller{
 
     function view(){
             $tag_name = $this->params[0];
-            $tag_id = get_one("SELECT tag_id FROM tag NATURAL JOIN user WHERE tag_name='$tag_name'");
+            $tag_id = get_first("SELECT tag_id FROM tag NATURAL JOIN user WHERE tag_name='$tag_name'");
             $this->posts = get_all("SELECT * FROM post_tags NATURAL JOIN post, user WHERE tag_id='$tag_id'");
         $post_id = $this->params[0];
         $this->post = get_one("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
