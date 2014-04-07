cd3584ba0ee67d294f6d3d30f3f689f0a70a421b
Gertrk <gert.rk1@hotmail.com>
2014-04-07 15:33:14 +0300

    Add framework files Customize project settings Rename welcome controller to posts Add posts listing functionality Add single post viewing functionality

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,12 +1,3 @@
-<h1>Welcome!</h1>
-<!-- Code for ajax -->
-<script type="text/javascript">
-    function clickme() {
-        $.post("<?=BASE_URL?>welcome", $( "#ajax-form" ).serialize(), function (data) {
-            $(".result").html(data);
-        });
-    }
-</script>
 <?foreach( $posts as $post ):?>
     <div class="span8">
 
@@ -14,8 +5,7 @@
         <p><?=$post["post_text"]?></p>
         <div>
         <span class="badge badge-success">Posted <?=$post["post created"]?></span>
-
-            </div>
+        <div class="pull-right">
     <? if (!empty($tags[$post['post_id']])) foreach ($tags   [$post	['post_id']] as $tag):?>
         <a href="<?=BASE_URL?>tags/view/<?=$tag['tag_id']?>"><span class = "label" style="background-color: #5bc0de"><?=$tag['tag_name']?></span></a>
     <?endforeach?>
