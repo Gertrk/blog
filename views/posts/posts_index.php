635cff25aabc8a39c96c216c2fe1b6e6921012c9
Gertrk <gert.rk1@hotmail.com>
2014-04-07 15:36:29 +0300

    Add framework files Customize project settings Rename welcome controller to posts Add posts listing functionality Add single post viewing functionality

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,12 +1,26 @@
+<h1>Welcome!</h1>
+<!-- Code for ajax -->
+<script type="text/javascript">
+    function clickme() {
+        $.post("<?=BASE_URL?>welcome", $( "#ajax-form" ).serialize(), function (data) {
+            $(".result").html(data);
+        });
+    }
+</script>
+
 <?foreach( $posts as $post ):?>
     <div class="span8">
 
         <h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post["post subject"]?></a></h1>
         <p><?=$post["post_text"]?></p>
         <div>
-        <span class="badge badge-success">Posted <?=$post["post created"]?></span>
-        <div class="pull-right">
-    <? if (!empty($tags[$post['post_id']])) foreach ($tags   [$post	['post_id']] as $tag):?>
-        <a href="<?=BASE_URL?>tags/view/<?=$tag['tag_id']?>"><span class = "label" style="background-color: #5bc0de"><?=$tag['tag_name']?></span></a>
-    <?endforeach?>
+            <span class="badge badge-success">Posted <?=$post["post created"]?></span>
+            <div class="pull-right">
+                <? if (!empty($tags[$post['post_id']])) foreach ($tags   [$post	['post_id']] as $tag):?>
+                    <a href="<?=BASE_URL?>tags/view/<?=$tag['tag']?>"><span class = "label" style="background-color: #5bc0de"><?=$tag['tag']?></span></a>
+                <?endforeach?>
+            </div>
+        </div>
+        <hr>
+    </div>
 <?endforeach?>
